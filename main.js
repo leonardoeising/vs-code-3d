eb9ecaab33e35a50900271ad9adcd73fab83f9ac
Leonardo <112594817+leonardoeising@users.noreply.github.com>
2024-05-10 09:28:37 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 const constantes = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date ("2024-05-01-10T00:00:00");
+const tempoObjetivo1 = new Date("2024-05-01-10T00:00:00");
 
-contadores[0].textContent = tempoObjetivo1;
+function calculaTempo(tempoObjetivo1){
+    let tempoAtual = newDate();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = math.floor(tempoFinal/1000);
+    let minutos = math.floor(segundos/60);
+    let horas = math.floor(minutos/60);
+    let dias = math.floor(horas/24);
+    return dias;
+}
+contadores[0].textContent = calculaTempo(tempoObjetivo);
 
 for (let i = 0; i < botoes.length; i++) {
     botoes[i].onclick = function () {
